ecfb43dc254b1edcb39c152c8a4711570e621529
Bertokeys1 <abarrientos93.ab@gmail.com>
2021-11-28 16:17:15 -0800

    added question var to test and also added jQuery. starting timer.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,58 @@
-var displayboxEl = ".display"
+var timerEl = document.getElementById('timer')
+var mainEl = document.getElementById('page')
+document.getElementById('start').addEventListener('click', start);
 
-var timerEl = document.getElementById(".countdown")
-var buttonEl = document.getElementById(".start-game")
+var startTime = 100;
 
+var questions = [
+    {
+        question = "what is Washingtons baseball team?",
+        multiChoice = [
+            "the Mermaids",
+            "The Dolphins",
+            "The Mariners",
+            "The walreses",
+        ],
+        answer: "the Mariners"
+    },
+    {
+        questions = "what is washingtons baseball team?",
+        multiChoice = [
+            "the Mermaids",
+            "The Dolphins",
+            "The Mariners",
+            "The walreses",
+        ],
+        answer: "the Mariners"
+    },
+
+];
+
+var contentBoxEl = document.getElementById('.content')
+
+var Welcome = document.createElement("h1");
+var instructions = document.createElement("h2");
+var startGame = document.createElement("button");
+
+Welcome.textContent = "Welcome to the baseball quiz!";
+
+instructions.textContent = "Hit the start button to test your baseball team knowledge!";
+
+startGame.textContent = "Start Game";
+
+contentBoxEl.appendChild(Welcome);
+contentBoxEl.appendChild(instructions);
+contentBoxEl.appendChild(startGame);
+
+welcome.className = "contentHeader";
+welcome.id = "welcomeHeader";
+instructions.setAttribute("style", "margin:auto; width:80%; padding:15px 0px; line-height: 1.5;");
+instructions.id = "instructions";
+startGame.id = "startGameButton";
+
+var startGameButton = document.querySelector('#startGameButton');
+var welcomeEl = document.querySelector('#welcomeHeader');
+var instructionsEl = document.querySelector('#instructions')
+
+var timer = document.getElementById('countdown');
+var timeLeft = 0;
